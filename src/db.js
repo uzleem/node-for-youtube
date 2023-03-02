4490f541999ca2b4e6c106332a92535380b7684e
uzleem <uzleem.github.com>
2023-03-02 19:17:15 +0900

    1

---
+++ src/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mon`goose";
+
+// mongoose 연결
+mongoose
+  .connect("mongodb://127.0.0.1:27017/youtube")
+  .catch((err) => console.log(err));
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connection to DB 🔥");
+const handleError = (error) => console.log("❌ DB Error", error);
+
+// on : 실행
+db.on("error", handleError);
+
+// once : 최초실행
+db.once("open", handleOpen);
